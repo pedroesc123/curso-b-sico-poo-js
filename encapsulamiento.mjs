3dcc02d01ccd4e88e1971b612c29146b0869a632
Pedro Espinoza Cordero <pedro.espinoza.c@uni.pe>
2021-07-15 14:46:08 -0500

    Agregado el encapsulamiento por modulos

--- encapsulamiento.mjs
+++ encapsulamiento.mjs
@@ -1,3 +1,31 @@
+function videoPlay(id){
+    const urlSecreta = "https://platziultrasecreto.com/" + id;
+    console.log("Se está reproduciendo desde la url" + urlSecreta);
+}
+
+function videoStop(id){
+    const urlSecreta = "https://platziultrasecreto.com/" + id;
+    console.log("Pausamos la url "+ urlSecreta);
+}
+
+export class PlatziClass {
+    constrcutor({
+        name,
+        videoID,
+    }){
+        this.name = name;
+        this.videoID = videoID;
+    }
+
+    reproducir() {
+        videoPlay(this.videoID);
+    }
+    pausar(){
+        videoStop(this.videoID);
+    }
+}
+
+
 class Course {
     constructor ({
         name,
---
+++ module.mjs
@@ -0,0 +1,9 @@
+import { PlatziClass } from "./encapsulamiento.mjs";
+
+const clase67 = new PlatziClass({
+    name: "JavaScript",
+    videoID: "asfasfasf",
+});
+
+clase67.reproducir();
+clase67.pausar();
